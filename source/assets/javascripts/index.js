b6a2d5aeae196365850a19bcbb1868328a962ffa
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-10 11:42:28 +0900

    filtering done for projects

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -11,5 +11,48 @@ const accordeon = () => {
 });
 }
 
-console.log(accordeon());
+accordeon();
 
+const filter = () => {
+  const buttons = document.querySelectorAll('.filter-btn');
+  const projects = document.querySelectorAll('.project');
+  buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      if(event.currentTarget.dataset.filter === 'all'){
+        const activeBtn = Array.from(document.querySelectorAll('.filter-btn')).find(element => element.classList.contains('active'));
+        activeBtn.classList.remove('active');
+        event.currentTarget.classList.add('active');
+        projects.forEach((element) => {
+          element.classList.contains('active') ? '' : element.classList.add('active');
+        })
+      } else if (event.currentTarget.dataset.filter === 'rails'){
+        const activeBtn = Array.from(document.querySelectorAll('.filter-btn')).find(element => element.classList.contains('active'));
+        activeBtn.classList.remove('active');
+        event.currentTarget.classList.add('active');
+        projects.forEach((element) => {
+          element.classList.remove('active');
+          element.dataset.filter === 'rails' ? element.classList.add('active') : '';
+        })
+      } else if (event.currentTarget.dataset.filter === 'react'){
+        const activeBtn = Array.from(document.querySelectorAll('.filter-btn')).find(element => element.classList.contains('active'));
+        activeBtn.classList.remove('active');
+        event.currentTarget.classList.add('active');
+        projects.forEach((element) => {
+          element.classList.remove('active');
+          element.dataset.filter === 'react' ? element.classList.add('active') : '';
+        })
+      } else if (event.currentTarget.dataset.filter === 'api'){
+        const activeBtn = Array.from(document.querySelectorAll('.filter-btn')).find(element => element.classList.contains('active'));
+        activeBtn.classList.remove('active');
+        event.currentTarget.classList.add('active');
+        projects.forEach((element) => {
+          element.classList.remove('active');
+          element.dataset.filter === 'api' ? element.classList.add('active') : '';
+        })
+      }
+    });
+  });
+}
+
+filter();
